d26b3eea978e99784c7a0368fd934276d304c3fe
Daniele Rizzieri <37268662+danirizziero@users.noreply.github.com>
2022-12-19 11:24:37 +0100

    Added auto-reconnection feature to data_source.py + code cleaning

--- data_source.py
+++ data_source.py
@@ -4,26 +4,61 @@ import random
 import socket
 import struct
 
-T_SAMPLE_ms = 30
-DATA_CARDINALITY = 3
+# TODO clean code and create input config file
+
+connection_error_string = "###########################################################################" \
+                          "\n###########################################################################" \
+                          "\nosPylloscope not connected. Continuing logging the samples.\n" \
+                          "###########################################################################" \
+                          "\n###########################################################################"
+conn_exceptions = (ConnectionAbortedError, ConnectionResetError, ConnectionRefusedError, TimeoutError)
+
+
+def connect_osPylloscope(HOST_, PORT_, DATA_CARDINALITY_):
+    so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    so.settimeout(0.5)
+    try:
+        print(f"Connecting to particle count server (@{HOST_}:{PORT_})...", end='', flush=True)
+        so.connect((HOST_, PORT_))
+        print(f"CONNECTED\n")
+        connected = True
+        packer_ = struct.Struct(f'{DATA_CARDINALITY_}f')
+        so.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, packer_.size)  # https://stackoverflow.com/a/30888505
+
+    except (ConnectionAbortedError, ConnectionResetError, ConnectionRefusedError, TimeoutError) as exception:
+        print("ERROR: Connection failed!")
+        raise exception
+    return so, connected
+
+
 if __name__ == '__main__':
+
+    #   Sampling configuration
+    T_SAMPLE_ms = 30
+    DATA_CARDINALITY = 3
+
     #   Socket configuration
     HOST = "127.0.0.1"  # check ipconfig /all to match proper ip add
     PORT = 4929  # arbitrary, chosen here, must match at the client side
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    packer = struct.Struct(f'{DATA_CARDINALITY}f')
-    print(packer.size)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, packer.size)  # https://stackoverflow.com/a/30888505
 
-    print(f"Connecting to particle count server (@{HOST}:{PORT})...", end='', flush=True)
-    s.connect((HOST, PORT))
-    print(f"CONNECTED\n")
+    # Necessary variables initialization
+    osPylloscope_OK = False
     cnt = 0
     data = [0] * DATA_CARDINALITY
     done = False
-    osPylloscope_OK = True
-    while True:
+    reconnect_cnt = 0
+    packer = struct.Struct(f'{DATA_CARDINALITY}f')
 
+    # First connection to osPylloscope
+    try:
+        s, osPylloscope_OK = connect_osPylloscope(HOST, PORT, DATA_CARDINALITY)
+    except conn_exceptions as e:
+        time.sleep(1)
+        print(connection_error_string)
+        time.sleep(1)
+
+    # Sampling
+    while True:
         for k in range(DATA_CARDINALITY):  # creating dummy data
             data[k] = 500 * random.random()
 
@@ -41,15 +76,13 @@ if __name__ == '__main__':
         try:
             if osPylloscope_OK:
                 s.sendall(packed_data)
-        except (ConnectionAbortedError, ConnectionResetError):
-            print("###########################################################################"
-                  "\n###########################################################################"
-                  "\n###########################################################################")
-            print("\nosPylloscope stopped receiving the samples. Continuing logging the samples.\n")
-            print("###########################################################################"
-                  "\n###########################################################################"
-                  "\n###########################################################################")
+            elif reconnect_cnt > 5 * 1000 / T_SAMPLE_ms:  # try to reconnect every 5 seconds
+                reconnect_cnt = 0
+                s, osPylloscope_OK = connect_osPylloscope(HOST, PORT, DATA_CARDINALITY)
+            else:
+                reconnect_cnt += 1
+        except conn_exceptions:
+            print(connection_error_string)
             osPylloscope_OK = False
-            # TODO try to reconnect to osPylloscope every 5 seconds without stopping the sampling
 
         time.sleep(T_SAMPLE_ms / 1000)
